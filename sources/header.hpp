d2f8a96a16fabc38d9b1e82e6ccd4c70bf938102
Alexey-Ershov <ershov_ae@mail.ru>
2019-11-19 17:24:33 +0300

    Implement simple greedy algorithm

--- sources/header.hpp
+++ sources/header.hpp
@@ -10,16 +10,32 @@
 #include <map>
 #include <filesystem>
 #include <functional>
+#include <sstream>
 
+
+struct Item
+{
+    unsigned num;
+    unsigned core_num;
+    unsigned ram;
+
+    Item(unsigned n, unsigned c_n, unsigned r)
+        : num {n},
+          core_num {c_n},
+          ram {r}
+    {}
+};
 
 // Vector of pairs with cores number and RAM.
-using CharactVect = std::vector<std::pair<int, int>>;
+using CharactVect = std::vector<Item>;
 
 
 struct ParsingResult
 {
     int conf_num;
     CharactVect charact_vect;
+
+    ParsingResult() = default;
 
     ParsingResult(const int&& cn, const CharactVect&& cv)
         : conf_num {cn},
@@ -50,11 +66,23 @@ private:
     std::string req_dir_;
     std::string serv_dir_;
 
+    // Internal XML data representation: vector of characteristics and
+    // number of configuration.
+    ParsingResult req_conf_;
+    ParsingResult serv_conf_;
+
+    unsigned first_available_serv_num_ = 0;
+
+    // Vector containing information about current loading of the servers.
+    CharactVect serv_load_;
+
     ParsingResult parse_xml_data(const std::string& input_file);
-    VmDeployment algorithm(ParsingResult& req_conf,
-                           ParsingResult& serv_conf);
+    VmDeployment algorithm();
     
     void print_depl_to_file(const VmDeployment& vm_depl);
+    bool try_deploy_vm(VmDeployment& vm_delp,
+                       unsigned vm_num,
+                       unsigned serv_num);
 };
 
 
--- sources/impl.cpp
+++ sources/impl.cpp
@@ -9,10 +9,15 @@ void RecourceDistributor::distributeRecources()
 {
     for (const auto& serv_file: fs::directory_iterator(serv_dir_)) {
         for (const auto& req_file: fs::directory_iterator(req_dir_)) {
-            auto req_conf = parse_xml_data(req_file.path().string());
-            auto serv_conf = parse_xml_data(serv_file.path().string());
+            req_conf_ = parse_xml_data(req_file.path().string());
+            serv_conf_ = parse_xml_data(serv_file.path().string());
 
-            print_depl_to_file(algorithm(req_conf, serv_conf));
+            // serv_load_ initialization.
+            for (unsigned i = 0; i < serv_conf_.charact_vect.size(); i++) {
+                serv_load_.emplace_back(Item(i, 0, 0));
+            }
+
+            print_depl_to_file(algorithm());
         }
     }
 }
@@ -28,34 +33,39 @@ RecourceDistributor::parse_xml_data(const std::string& input_file)
     CharactVect output_vect;
     
     auto conf = doc.child("configuration");
+    unsigned i = 0;
     for (const auto& node: conf) {
-        output_vect.emplace_back(std::make_pair(
+        output_vect.emplace_back(Item(
+                i,
                 node.attribute("core_num").as_int(),
                 node.attribute("ram").as_int()));
+
+        i += 1;
     }
 
     return ParsingResult(std::move(conf.attribute("n").as_int()),
                          std::move(output_vect));
 }
 
-VmDeployment RecourceDistributor::algorithm(ParsingResult& req_conf,
-                                            ParsingResult& serv_conf)
+VmDeployment RecourceDistributor::algorithm()
 {
     // <debug_log>
     std::cerr << "\n\n=== REQUESTS ===" << std::endl;
-    std::cerr << "req_conf_num = " << req_conf.conf_num << std::endl;
-    for (const auto& req: req_conf.charact_vect) {
-        std::cerr << req.first << " : "
-                  << req.second
+    std::cerr << "req_conf_num = " << req_conf_.conf_num << std::endl;
+    for (const auto& req: req_conf_.charact_vect) {
+        std::cerr << "#" << req.num << "   "
+                  << req.core_num << " : "
+                  << req.ram
                   << std::endl;
     }
     std::cerr << std::endl;
 
     std::cerr << "=== SERVERS ===" << std::endl;
-    std::cerr << "serv_conf_num = " << serv_conf.conf_num << std::endl;
-    for (const auto& serv: serv_conf.charact_vect) {
-        std::cerr << serv.first << " : "
-                  << serv.second
+    std::cerr << "serv_conf_num = " << serv_conf_.conf_num << std::endl;
+    for (const auto& serv: serv_conf_.charact_vect) {
+        std::cerr << "#" << serv.num << "   "
+                  << serv.core_num << " : "
+                  << serv.ram
                   << std::endl;
     }
     // </debug_log>
@@ -65,73 +75,157 @@ VmDeployment RecourceDistributor::algorithm(ParsingResult& req_conf,
     unsigned sum_ram_vm = 0;
     unsigned sum_core_num_serv = 0;
     unsigned sum_ram_serv = 0;
-    for (const auto& req: req_conf.charact_vect) {
-        sum_core_num_vm += req.first;
-        sum_ram_vm += req.second;
+    for (const auto& req: req_conf_.charact_vect) {
+        sum_core_num_vm += req.core_num;
+        sum_ram_vm += req.ram;
     }
 
-    for (const auto& serv: serv_conf.charact_vect) {
-        sum_core_num_serv += serv.first;
-        sum_ram_serv += serv.second;
+    for (const auto& serv: serv_conf_.charact_vect) {
+        sum_core_num_serv += serv.core_num;
+        sum_ram_serv += serv.ram;
     }
 
     if (static_cast<double>(sum_core_num_serv) / sum_core_num_vm <
         static_cast<double>(sum_ram_serv) / sum_ram_vm) {
 
-        auto comparator = [](auto& left, auto& right)
-                          {
-                              return left.first > right.first;
-                          };
+        auto comparator_less = [](Item& left, Item& right)
+                               {
+                                   return left.core_num < right.core_num;
+                               };
 
-        std::sort(req_conf.charact_vect.begin(),
-                  req_conf.charact_vect.end(),
-                  comparator);
+        auto comparator_gr = [](Item& left, Item& right)
+                             {
+                                 return left.core_num > right.core_num;
+                             };
 
-        std::sort(serv_conf.charact_vect.begin(),
-                  serv_conf.charact_vect.end(),
-                  comparator);
+        std::sort(req_conf_.charact_vect.begin(),
+                  req_conf_.charact_vect.end(),
+                  comparator_less);
+
+        std::sort(serv_conf_.charact_vect.begin(),
+                  serv_conf_.charact_vect.end(),
+                  comparator_gr);
     
     } else {
-        auto comparator = [](auto& left, auto& right)
-                          {
-                              return left.second > right.second;
-                          };
+        auto comparator_less = [](Item& left, Item& right)
+                               {
+                                   return left.ram < right.ram;
+                               };
 
-        std::sort(req_conf.charact_vect.begin(),
-                  req_conf.charact_vect.end(),
-                  comparator);
+        auto comparator_gr = [](Item& left, Item& right)
+                             {
+                                 return left.ram > right.ram;
+                             };
 
-        std::sort(serv_conf.charact_vect.begin(),
-                  serv_conf.charact_vect.end(),
-                  comparator);
+        std::sort(req_conf_.charact_vect.begin(),
+                  req_conf_.charact_vect.end(),
+                  comparator_less);
+
+        std::sort(serv_conf_.charact_vect.begin(),
+                  serv_conf_.charact_vect.end(),
+                  comparator_gr);
     }
 
     // <debug_log>
     std::cerr << "\n\n=== REQUESTS ===" << std::endl;
-    std::cerr << "req_conf_num = " << req_conf.conf_num << std::endl;
-    for (const auto& req: req_conf.charact_vect) {
-        std::cerr << req.first << " : "
-                  << req.second
+    std::cerr << "req_conf_num = " << req_conf_.conf_num << std::endl;
+    for (const auto& req: req_conf_.charact_vect) {
+        std::cerr << "#" << req.num << "   "
+                  << req.core_num << " : "
+                  << req.ram
                   << std::endl;
     }
     std::cerr << std::endl;
 
     std::cerr << "=== SERVERS ===" << std::endl;
-    std::cerr << "serv_conf_num = " << serv_conf.conf_num << std::endl;
-    for (const auto& serv: serv_conf.charact_vect) {
-        std::cerr << serv.first << " : "
-                  << serv.second
+    std::cerr << "serv_conf_num = " << serv_conf_.conf_num << std::endl;
+    for (const auto& serv: serv_conf_.charact_vect) {
+        std::cerr << "#" << serv.num << "   "
+                  << serv.core_num << " : "
+                  << serv.ram
                   << std::endl;
     }
     // </debug_log>
+
+    VmDeployment vm_depl;    
+
+    // Greedy algorithm.
+    for (unsigned i = 0; i < req_conf_.charact_vect.size(); i++) {
+        unsigned j = 0;
+        bool was_depl_succ = false;
+        while (j < serv_conf_.charact_vect.size() and !was_depl_succ) {
+            was_depl_succ = try_deploy_vm(vm_depl, i, j);
+            if (was_depl_succ) {
+                vm_depl.deployed_vm_num += 1;
+                if (vm_depl.deployed_vm_num == req_conf_.charact_vect.size()) {
+                    vm_depl.was_all_vm_deployed = true;
+                }
+            }
+            j += 1;
+        }
+    }
+
+
     
 
 
 
-    return VmDeployment();
+    return vm_depl;
 }
     
 void RecourceDistributor::print_depl_to_file(const VmDeployment& vm_depl)
 {
+    std::cerr << "\n\n" << "=== DEPL ===" << std::endl;
 
+    for (const auto& it: vm_depl.vm_mapping) {
+        std::cerr << it.first << " -> " << it.second << std::endl;
+    }
+
+    std::stringstream ss;
+    ss << "Number of deployed VM: "
+       << vm_depl.deployed_vm_num;
+
+    std::stringstream dashes;
+    for (unsigned i = 0; i < ss.str().length(); i++) {
+        dashes << "-";
+    }
+
+    std::cerr << dashes.str() << std::endl
+              << ss.str() << std::endl;
+
+    if (vm_depl.was_all_vm_deployed) {
+        std::cerr << "All VM deployed: True" << std::endl;
+    
+    } else {
+        std::cerr << "All VM deployed: False" << std::endl;
+    }
+
+    std::cerr << dashes.str() << std::endl;
+}
+
+bool RecourceDistributor::try_deploy_vm(VmDeployment& vm_depl,
+                                        unsigned vm_num,
+                                        unsigned serv_num)
+{
+    if (serv_load_[serv_num].core_num +
+        req_conf_.charact_vect[vm_num].core_num <=
+        serv_conf_.charact_vect[serv_num].core_num
+        and
+        serv_load_[serv_num].ram +
+        req_conf_.charact_vect[vm_num].ram <=
+        serv_conf_.charact_vect[serv_num].ram) {
+        
+        vm_depl.vm_mapping[req_conf_.charact_vect[vm_num].num] =
+                serv_conf_.charact_vect[serv_num].num;
+        
+        serv_load_[serv_num].core_num +=
+                req_conf_.charact_vect[vm_num].core_num;
+        
+        serv_load_[serv_num].ram += req_conf_.charact_vect[vm_num].ram;
+
+        return true;
+    
+    } else {
+        return false;
+    }
 }
